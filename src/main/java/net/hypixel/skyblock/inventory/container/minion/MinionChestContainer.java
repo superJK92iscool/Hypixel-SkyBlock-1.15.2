57c74d5b9c1da7473b80b90bfd11085fa8fe6eeb
MrPineapple070 <74255810+MrPineapple070@users.noreply.github.com>
2020-11-11 16:51:43 -0600

    Update MinionChestContainer.java

--- src/main/java/net/hypixel/skyblock/inventory/container/minion/MinionChestContainer.java
+++ src/main/java/net/hypixel/skyblock/inventory/container/minion/MinionChestContainer.java
@@ -2,81 +2,79 @@ package net.hypixel.skyblock.inventory.container.minion;
 
 import javax.annotation.Nonnull;
 
-import net.hypixel.skyblock.init.blocks.BlockInit;
+import net.hypixel.skyblock.blocks.minion.MinionChestBlock.ChestType;
 import net.hypixel.skyblock.inventory.container.ModContainerTypes;
-import net.hypixel.skyblock.tileentity.minion.AbstractMinionChestTileEntity;
-import net.hypixel.skyblock.tileentity.minion.AbstractMinionChestTileEntity.LargeMCTE;
-import net.hypixel.skyblock.tileentity.minion.AbstractMinionChestTileEntity.MediumMCTE;
-import net.hypixel.skyblock.tileentity.minion.AbstractMinionChestTileEntity.SmallMCTE;
-import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.util.IWorldPosCallable;
 
 /**
  * Serve as a basis for all {@code MinionChestContainer}.<br>
- * <a href="https://hypixel-skyblock.fandom.com/wiki/Minion_Chests">Click here
- * for a discription of Minion Chests.</a>
+ * <a href="https://hypixel-skyblock.fandom.com/wiki/Minion_Chests">Click
+ * here</a> for a description of Minion Chests.
  *
  * @author MrPineapple070
  * @version 12 June 2020
  */
 public abstract class MinionChestContainer extends Container {
 	public static class LargeMCC extends MinionChestContainer {
-		public LargeMCC(int windowId, PlayerInventory pInvIn, AbstractMinionChestTileEntity tileEntity) {
-			super(ModContainerTypes.large_mcc.get(), windowId, pInvIn, tileEntity);
+		public LargeMCC(int windowId, PlayerInventory pInvIn) {
+			super(ModContainerTypes.large_mcc.get(), windowId, pInvIn, ChestType.LARGE);
 		}
 
 		public LargeMCC(int windowId, PlayerInventory pInvIn, PacketBuffer data) {
-			this(windowId, pInvIn, new LargeMCTE());
+			this(windowId, pInvIn);
 		}
 	}
 
 	public static class MediumMCC extends MinionChestContainer {
-		public MediumMCC(int windowId, PlayerInventory pInvIn, AbstractMinionChestTileEntity tileEntity) {
-			super(ModContainerTypes.medium_mcc.get(), windowId, pInvIn, tileEntity);
+		public MediumMCC(int windowId, PlayerInventory pInvIn) {
+			super(ModContainerTypes.medium_mcc.get(), windowId, pInvIn, ChestType.MEDIUM);
 		}
 
 		public MediumMCC(int windowId, PlayerInventory pInvIn, PacketBuffer data) {
-			this(windowId, pInvIn, new MediumMCTE());
+			this(windowId, pInvIn);
 		}
 	}
 
 	public static class SmallMCC extends MinionChestContainer {
-		public SmallMCC(int windowId, PlayerInventory pInvIn, AbstractMinionChestTileEntity tileEntity) {
-			super(ModContainerTypes.small_mcc.get(), windowId, pInvIn, tileEntity);
+		public SmallMCC(int windowId, PlayerInventory pInvIn) {
+			super(ModContainerTypes.small_mcc.get(), windowId, pInvIn, ChestType.SMALL);
 		}
 
 		public SmallMCC(int windowId, PlayerInventory pInvIn, PacketBuffer data) {
-			this(windowId, pInvIn, new SmallMCTE());
+			this(windowId, pInvIn);
 		}
 	}
 
 	/**
-	 * The {@link IWorldPosCallable} for this.
+	 * {@link IInventory} of the contents of this.
 	 */
 	@Nonnull
-	protected IWorldPosCallable canInteractWithCallable;
+	protected final IInventory chestInventory;
 
 	/**
-	 * The {@link AbstractMinionChestTileEntity} for this.
+	 * {@link ChestType} of this.
 	 */
 	@Nonnull
-	public AbstractMinionChestTileEntity tileEntity;
+	protected final ChestType type;
 
 	protected MinionChestContainer(ContainerType<? extends MinionChestContainer> typeIn, int windowId,
-			PlayerInventory pInvIn, AbstractMinionChestTileEntity tileEntity) {
+			PlayerInventory pInvIn, ChestType type) {
 		super(typeIn, windowId);
-		this.tileEntity = tileEntity;
-		this.canInteractWithCallable = IWorldPosCallable.of(tileEntity.getWorld(), tileEntity.getPos());
+		this.type = type;
+		this.chestInventory = new Inventory(this.type.additional);
+		this.chestInventory.openInventory(pInvIn.player);
 
 		// Main Inventory
 		int rowMin, rowMax, colMin, colMax;
-		switch (this.tileEntity.getChestType()) {
+		switch (type) {
 		case SMALL:
 			rowMin = 1;
 			rowMax = 2;
@@ -96,14 +94,13 @@ public abstract class MinionChestContainer extends Container {
 			colMax = 5;
 			break;
 		default:
-			throw new IllegalStateException(
-					"Illegal AbstractMinionChestTileEntity.Type " + this.tileEntity.getChestType().name());
+			throw new IllegalStateException("Illegal ChestType " + this.type.name());
 		}
-
+		
+		//Main Inventory
 		for (int row = rowMin; row < rowMax; ++row)
-			for (int col = colMin; col < colMax; ++col) {
-				this.addSlot(new MinionChestSlot(this.tileEntity, row, col, rowMax));
-			}
+			for (int col = colMin; col < colMax; ++col)
+				this.addSlot(new MinionChestSlot(this.chestInventory, row, col, rowMax));
 
 		// Player Inventory
 		for (int row = 0; row < 3; ++row)
@@ -117,16 +114,37 @@ public abstract class MinionChestContainer extends Container {
 
 	@Override
 	public boolean canInteractWith(PlayerEntity playerIn) {
-		Block block;
-		switch (this.tileEntity.getChestType()) {
-		case SMALL:
-		default:
-			block = BlockInit.small_mcb.get();
-		case MEDIUM:
-			block = BlockInit.medium_mcb.get();
-		case LARGE:
-			block = BlockInit.large_mcb.get();
+		return this.chestInventory.isUsableByPlayer(playerIn);
+	}
+
+	public IInventory getChestInventory() {
+		return this.chestInventory;
+	}
+
+	@Override
+	public void onContainerClosed(PlayerEntity playerIn) {
+		super.onContainerClosed(playerIn);
+		this.chestInventory.closeInventory(playerIn);
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(PlayerEntity playerIn, int index) {
+		ItemStack itemstack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+			if (index < this.type.additional)
+				if (!this.mergeItemStack(itemstack1, this.type.additional, this.inventorySlots.size(), true))
+					return ItemStack.EMPTY;
+				else if (!this.mergeItemStack(itemstack1, 0, this.type.additional, false))
+					return ItemStack.EMPTY;
+
+			if (itemstack1.isEmpty())
+				slot.putStack(ItemStack.EMPTY);
+			else
+				slot.onSlotChanged();
 		}
-		return isWithinUsableDistance(this.canInteractWithCallable, playerIn, block);
+		return itemstack;
 	}
 }
